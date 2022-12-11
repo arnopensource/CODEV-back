44f305a13f822cea8c00c7e0b92bdcbfd828e5ab
arno <arno@dupere.me>
2022-12-11 18:08:32 +0100

    added login via nfc

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/joho/godotenv"
 	"github.com/supabase-community/gotrue-go"
 	"github.com/supabase-community/gotrue-go/types"
+
+	_ "github.com/lib/pq"
 )
 
 var (
 	supabaseProjectRef string
 	supabaseAPIKey     string
+	datasource         string
+	jwtSecret          string
 )
 
 func init() {
@@ -25,13 +32,21 @@ func init() {
 
 	supabaseProjectRef = os.Getenv("SUPABASE_PROJECT_REF")
 	supabaseAPIKey = os.Getenv("SUPABASE_API_KEY")
+	datasource = os.Getenv("DATASOURCE")
+	jwtSecret = os.Getenv("JWT_SECRET")
 
-	if supabaseProjectRef == "" || supabaseAPIKey == "" {
+	if supabaseProjectRef == "" || supabaseAPIKey == "" || datasource == "" || jwtSecret == "" {
 		log.Fatal(".env file is incomplete")
 	}
 }
 
 func main() {
+	db, err := sql.Open("postgres", datasource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	authClient := gotrue.New(supabaseProjectRef, supabaseAPIKey)
 
 	r := gin.Default()
@@ -64,6 +79,25 @@ func main() {
 		}
 		c.JSON(http.StatusOK, resp.AccessToken)
 	})
+	r.POST("/nfc/login", func(c *gin.Context) {
+		var body NFCLoginBody
+		c.MustBindWith(&body, binding.JSON)
+		var userID string
+		err = db.QueryRowContext(context.Background(), "SELECT id FROM auth.users WHERE raw_user_meta_data->>'nfc' = $1", body.NFC).Scan(&userID)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"aud": "authenticated",
+			"sub": userID,
+		}).SignedString([]byte(jwtSecret))
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		c.JSON(http.StatusOK, token)
+	})
 
 	r.Run()
 }
@@ -77,4 +111,8 @@ type SignupBody struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	NFC      string `json:"nfc" binding:"required"`
+}
+
+type NFCLoginBody struct {
+	NFC string `json:"nfc" binding:"required"`
 }
